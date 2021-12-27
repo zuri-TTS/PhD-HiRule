7f5d50acf2890f35e151ea477f3b3033b2be2f29
zuri <webzuri@gmail.com>
2021-12-27 17:50:59 +0100

    Summary, add config "summary.type"

--- src/main/java/insomnia/demo/input/Summary.java
+++ src/main/java/insomnia/demo/input/Summary.java
@@ -1,16 +1,19 @@
 package insomnia.demo.input;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.text.ParseException;
 
 import org.apache.commons.configuration2.Configuration;
 
 import insomnia.demo.TheConfiguration;
 import insomnia.demo.input.InputData.Filters;
+import insomnia.implem.kv.KV;
 import insomnia.implem.kv.data.KVLabel;
 import insomnia.implem.kv.data.KVLabels;
 import insomnia.implem.summary.LabelSummary;
 import insomnia.implem.summary.LabelSummaryReader;
+import insomnia.implem.summary.PathSummary;
 import insomnia.summary.ISummary;
 
 public final class Summary
@@ -22,10 +25,11 @@ public final class Summary
 
 	public static ISummary<Object, KVLabel> get(Configuration config) throws IOException, ParseException
 	{
-		return get(config.getString(TheConfiguration.OneProperty.Summary.getPropertyName()));
+		return get(config.getString(TheConfiguration.OneProperty.Summary.getPropertyName()), //
+			config.getString("summary.type").equals("path"));
 	}
 
-	public static ISummary<Object, KVLabel> get(String uri) throws IOException, ParseException
+	private static ISummary<Object, KVLabel> get(String uri, boolean path) throws IOException, ParseException
 	{
 		if (uri.isEmpty())
 			return LabelSummary.create();
@@ -35,7 +39,16 @@ public final class Summary
 		if (optPath.isEmpty())
 			throw new IllegalArgumentException(String.format("The summary file: `%s` does not exists", uri));
 
-		var data = InputData.filters(InputData.getLinesOf(optPath.get()), Filters.NO_BLANK);
-		return new LabelSummaryReader<Object, KVLabel>().setReadLabel(KVLabels::parseIllegal).read(data.iterator());
+		if (path)
+		{
+			var s = PathSummary.<Object, KVLabel>create();
+			s.addTree(KV.treeFromString(Files.readString(optPath.get())));
+			return s;
+		}
+		else
+		{
+			var data = InputData.filters(InputData.getLinesOf(optPath.get()), Filters.NO_BLANK);
+			return new LabelSummaryReader<Object, KVLabel>().setReadLabel(KVLabels::parseIllegal).read(data.iterator());
+		}
 	}
 }
